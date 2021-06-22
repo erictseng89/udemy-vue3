593d2f96682eeaacc8525bf12801d41e41a4291d
Eric Tseng <erictseng89@gmail.com>
2021-06-22 06:41:50 +0000

    ep06: create vue app, assign data and methods, v-model, v-on:event, v-for, .mount

--- app.js
+++ app.js
@@ -1,36 +1,16 @@
-/**
- * Create constants for the elements
- */
+let app = Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enteredValue: '',
+    };
+  },
+  methods: {
+    addGoals() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = ''
+    }
+  }
+});
 
-const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const listEl = document.querySelector('ul');
-
-/**
- * Create the add goal function for the click event.
- * 
- * ! inputElement.value  
- * The <input> element has many attributes due to having multiple 'types'. The .value
- * in a type=text is whatever is inside the textbox.
- * 
- * ! liElement.textContent  
- * The textContent of a 'textnode' returns the text inside the node, which includes the
- * <script> and <style> of all its elements. It is different from .innerText as that only returns the human readable text.
- *
- */
-
-function addGoal() {
-  const inputValue = inputEl.value;
-  const newItem = document.createElement('li');
-  newItem.textContent = inputValue;
-  listEl.appendChild(newItem);
-  inputEl.value = '';
-}
-
-/**
- * Create a listener for the button on click. The event should not trigger the function,
- * hence do not add the () at the end of the function.
- */
-buttonEl.addEventListener('click', addGoal)
-
-
+app.mount('#app');
