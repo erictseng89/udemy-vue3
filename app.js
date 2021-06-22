a696b8fe0016b476745916e20ad161048f6348b2
Eric Tseng <erictseng89@gmail.com>
2021-06-22 09:13:08 +0000

    ep20: v-on:event="" event listener

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 const app = Vue.createApp({
   data() {
     return {
-      name: 'ted',
-      age: 30,
-      randNumber: Math.random(),
-      link: 'https://cdn.nohat.cc/thumb/f/720/4996837261967360.jpg'
+      counter: 0
     }
   },
   methods: {
-    randMethod() {
-      return Math.random();
-    },
+
   }
 })
 
-app.mount('#assignment');
+app.mount('#events');
