8408d2d6cd9e728b2e888abce0a35bbfa382c679
Eric Tseng <erictseng89@gmail.com>
2021-06-23 09:27:15 +0000

    ep50-downloaded files

--- app.js
+++ app.js
@@ -1,24 +1,15 @@
 const app = Vue.createApp({
   data() {
     return {
-      enteredTaskValue: '',
-      tasks: [],
-      listVisibility: true,
+
     }
   },
   computed: {
-    listVisibilityButton() {
-      return this.listVisibility ? 'Hide List' : 'Show List';
-    }
+
   },
   methods: {
-    addTask() {
-      this.tasks.push(this.enteredTaskValue);
-    },
-    toggleListVisibility() {
-      this.listVisibility = !this.listVisibility;
-    }
-  }
+
+  },
 });
 
-app.mount('#assignment');
+app.mount('#game');
