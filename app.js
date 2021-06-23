b1d586e1a6ee20b2c8f8543e89c050585f366b3f
Eric Tseng <erictseng89@gmail.com>
2021-06-23 03:56:05 +0000

    ep32: v-bind style and class as an object can include ternary operator

--- app.js
+++ app.js
@@ -1,44 +1,23 @@
 const app = Vue.createApp({
   data() {
     return {
-      number: 0,
-      message: [
-        'Not there yet',
-        'Too much!',
-      ],
-    }
-  },
-  /**
-   * You can watch computed properties as well as data properties. Use the const that =
-   * this trick to use 'this' in nested functions.
-   */
-  watch: {
-    result() {
-      const that = this;
-      setTimeout(() => {
-        that.number = 0;
-      }, 5000);
-    }
-  },
-  computed: {
-    result() {
-      if (this.number < 37) {
-        return this.message[0];
-      } else if (this.number === 37) {
-        return this.number;
-      } else {
-        return this.message[1];
-      }
-    }
+      boxASelected: false,
+      boxBSelected: false,
+      boxCSelected: false,
+    };
   },
   methods: {
-    addFive() {
-      this.number = this.number + 5;
-    },
-    addOne() {
-      this.number = this.number + 1;
-    },
+    selectBox(box) {
+      if (box === 'A') {
+        console.log('triggered');
+        this.boxASelected = true;
+      } else if (box === 'B') {
+        this.boxBSelected = true;
+      } else if (box === 'C') {
+        this.boxCSelected = true;
+      }
+    }
   }
-});
+})
 
-app.mount('#assignment');
+app.mount('#styling');
