6a35a94f9bbc06f00472abae38c63034b26a16e5
Eric Tseng <erictseng89@gmail.com>
2021-06-28 08:17:47 +0000

    module11:initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,96 +1,28 @@
 <template>
-  <the-header></the-header>
-  <main>
-    <selection-tab
-      @select-tab="changeContent"
-      :tabChoices="tabChoices"
-      :activeTab="activeTab"
-    ></selection-tab>
-    <keep-alive>
-      <component
-        :is="activeTab"
-        :list="resourceList"
-        @add-resource="addResource"
-      ></component>
-    </keep-alive>
-  </main>
+  <the-form></the-form>
 </template>
 
 <script>
-import TheHeader from "./components/layout/TheHeader.vue";
-import SelectionTab from "./components/layout/SelectionTab.vue";
-import StoredResources from "./components/content/StoredResources.vue";
-import AddResource from "./components/content/AddResource.vue";
+import TheForm from './components/TheForm.vue';
 
 export default {
   components: {
-    TheHeader,
-    SelectionTab,
-    StoredResources,
-    AddResource,
-  },
-  data() {
-    return {
-      tabChoices: [
-        {
-          name: "Stored Resources",
-          tagName: "stored-resources",
-        },
-        {
-          name: "Add Resource",
-          tagName: "add-resource",
-        },
-      ],
-      resourceList: [
-        {
-          id: "official",
-          title: "Official Guide",
-          description: "The official Vue.js documentation",
-          link: "https://v3.vuejs.org/guide/introduction.html",
-        },
-        {
-          id: "google",
-          title: "Google",
-          description: "learn to google...",
-          link: "https://www.google.com",
-        },
-      ],
-      activeTab: "stored-resources",
-    };
-  },
-  methods: {
-    addResource(title, description, link) {
-      const newResource = {
-        id: new Date().toISOString(),
-        title: title,
-        description: description,
-        link: link,
-      };
-      this.resourceList.push(newResource);
-    },
-    changeContent(tagName) {
-      this.activeTab = tagName;
-    },
-    deleteResource(id) {
-      this.resourceList = this.resourceList.filter((item) => item.id !== id);
-    },
-  },
-  provide() {
-    return {
-      deleteResource: this.deleteResource,
-    };
-  },
-};
+    TheForm
+  }  
+}
 </script>
 
 <style>
-body {
+* {
+  box-sizing: border-box;
+}
+
+html {
   font-family: sans-serif;
 }
-main {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 2rem;
+
+body {
+  margin: 0;
+  background-color: #292929;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,5 @@
 import { createApp } from 'vue';
 
-import App from './App.vue'
-const app = createApp(App)
+import App from './App.vue';
 
-import BaseCard from './components/layout/BaseCard.vue';
-
-app.component('base-card', BaseCard);
-// app.component('alert-error', AlertError);
-
-app.mount('#app');
+createApp(App).mount('#app');
